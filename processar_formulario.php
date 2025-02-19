84b701c5d30328d3341473420db5b1524edd5bcb
Eros Machado <112194183+ErosMachado@users.noreply.github.com>
2025-02-19 00:07:29 -0300

    Create processar_formulario.php

---
+++ processar_formulario.php
@@ -0,0 +1,36 @@
+<?php
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    // Obtém os dados do formulário
+    $nome = htmlspecialchars($_POST['name']);
+    $email = filter_var($_POST['email'], FILTER_SANITIZE_EMAIL);
+    $mensagem = htmlspecialchars($_POST['Mesage']);
+
+    // Validação básica
+    if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+        echo "E-mail inválido.";
+        exit;
+    }
+
+    // Configuração do e-mail
+    $to = "email@email.com"; // Seu e-mail
+    $subject = "Mensagem do formulário de contato";
+    $headers = "From: $email\r\n";
+    $headers .= "Reply-To: $email\r\n";
+    $headers .= "Content-Type: text/plain; charset=UTF-8\r\n";
+
+    // Corpo do e-mail
+    $body = "Você recebeu uma nova mensagem:\n\n";
+    $body .= "Nome: $nome\n";
+    $body .= "E-mail: $email\n";
+    $body .= "Mensagem:\n$mensagem\n";
+
+    // Envia o e-mail
+    if (mail($to, $subject, $body, $headers)) {
+        echo "Mensagem enviada com sucesso!";
+    } else {
+        echo "Erro ao enviar a mensagem.";
+    }
+} else {
+    echo "Método de requisição inválido.";
+}
+?>
